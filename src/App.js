0d558aaddeb3ae34106f663384d6e35220268687
Kelly Nelson <nelson20@gmail.com>
2017-02-12 21:50:37 -0800

    fix props flow, style property title & value boxes

--- src/App.js
+++ src/App.js
@@ -11,15 +11,16 @@ class PropertyName extends React.Component {
   // This should show the names of each monopoly Property and include their colors via JSON className property.
   render() {
 
-    const name = this.props.propertyNode.name.slice(0,3) // Showing just the first three letters of Property name.
+    const name = this.props.property.name.slice(0,3) // Showing just the first three letters of Property name.
 
-    return 
+    return (
       
-      <div className={this.props.propertyNode.className}>
-        <div className="prop-title-container">
-          {name}
+      <div className="property-title-container pull-left">
+        <div className={this.props.property.className}>
+          <p>{name}</p>
         </div>
       </div>  
+    )
   }
 }
 
@@ -29,23 +30,23 @@ class PropertyValues extends React.Component {
   // This component should show a drop down menu to select the state (mortgaged, houses, etc) of the Property at the end of the game
   render() {
 
-    const { owned } = this.props.propertyNode;
-    const { mortgaged } = this.props.propertyNode;
-    const { houseCost } = this.props.propertyNode;
+    const { owned } = this.props.property;
+    const { mortgaged } = this.props.property;
+    const { houseCost } = this.props.property;
 
-    return 
-      
-      <div className="prop-values-container">
-        <DropdownButton title="$">
-          <button eventKey="o" value={owned}>O</button>
-          <button eventKey="m" value={mortgaged}>M</button>
-          <button className="green btn-selections" eventKey="1" value={owned + (houseCost * 1)}>1</button>
-          <button className="green btn-selections" eventKey="2" value={owned + (houseCost * 2)}>2</button>
-          <button className="green btn-selections" eventKey="3" value={owned + (houseCost * 3)}>3</button>
-          <button className="green btn-selections" eventKey="4" value={owned + (houseCost * 4)}>4</button>
-          <button className="red btn-selections" eventKey="h" value={owned + (houseCost * 5)}>H</button>
+    return (
+      <div className="property-values-container">
+        <DropdownButton className="ddown" id="ddown" title="$">
+          <MenuItem className="owned-box btn-selections" eventKey="o" value={owned}>O</MenuItem>
+          <MenuItem className="mortgaged-box btn-selections" eventKey="m" value={mortgaged}>M</MenuItem>
+          <MenuItem className="green btn-selections" eventKey="1" value={owned + (houseCost * 1)}>1</MenuItem>
+          <MenuItem className="green btn-selections" eventKey="2" value={owned + (houseCost * 2)}>2</MenuItem>
+          <MenuItem className="green btn-selections" eventKey="3" value={owned + (houseCost * 3)}>3</MenuItem>
+          <MenuItem className="green btn-selections" eventKey="4" value={owned + (houseCost * 4)}>4</MenuItem>
+          <MenuItem className="red btn-selections" eventKey="h" value={owned + (houseCost * 5)}>H</MenuItem>
         </DropdownButton>
       </div>
+    )
   }
 }
 
@@ -54,7 +55,9 @@ class PropertyValuesTotal extends React.Component {
   // This should show the user the total valuation of the property once they select the drop down menu
   render() {
 
-    return 
+    return (
+        <div></div>
+      )
         // need code to add up selection from <PropertyValues /> and display in a div
   }
 }
@@ -64,13 +67,17 @@ class Property extends React.Component {
   // This should be a child to <App />
   render() {
 
+    const { property } = this.props
+
     return (
       
       <div className="container">
-        <div className="row">
-          <PropertyName key={this.props.propertyNode.name}/>
-          <PropertyValues key={this.props.propertyNode.houseCost}/>
-          <PropertyValuesTotal />
+        <div className="row properties-item">
+          <div className="col-xs-12">
+            <PropertyName property={property}/>
+            <PropertyValues property={property}/>
+            <PropertyValuesTotal />
+          </div>
         </div>
       </div> 
     );
@@ -83,10 +90,8 @@ class App extends React.Component {
   render() {
 
     const propertyNode = data.properties.map(function(property) {
-      // Trying to bring in data about each property from JSON file.
-      // Not sure where this should get put or the exact code that brings in the data.
       return (
-        <Property key={property.id} children={property} />
+        <Property key={property.id} property={property} />
       );
     });
 
@@ -104,87 +109,5 @@ class App extends React.Component {
   }
 }
 
-// This is a temporary use of JS Object instead of JSON data file for troubleshooting.
-// var properties = [
-//   {
-//     id: 1,
-//     className: 'brown',
-//     name: 'Baltic Avenue',
-//     owned: 60,
-//     mortgaged: 30,
-//     houseCost: 50
-//   },
-//   {
-//     id: 2,
-//     className: 'light-blue',
-//     name: 'Oriental Avenue',
-//     owned: 100,
-//     mortgaged: 50,
-//     houseCost: 50
-//   },
-//   {
-//     id: 3,
-//     className: 'purple',
-//     name: 'St. Charles Place',
-//     owned: 140,
-//     mortgaged: 70,
-//     houseCost: 100
-//   }
-// ];
-
-
-
 export default App;
 
-// The following is a different approach I tried
-
-// class App extends React.Component {
-//   // This is the main container component
-// constructor() {
-//   super();
-
-//   this.state = {
-//     properties:
-//     [{
-//       id: 1,
-//       className: 'brown',
-//       name: 'Baltic Avenue',
-//       owned: 60,
-//       mortgaged: 30,
-//       houseCost: 50
-//     },
-//     {
-//       id: 2,
-//       className: 'light-blue',
-//       name: 'Oriental Avenue',
-//       owned: 100,
-//       mortgaged: 50,
-//       houseCost: 50
-//     },
-//     {
-//       id: 3,
-//       className: 'purple',
-//       name: 'St. Charles Place',
-//       owned: 140,
-//       mortgaged: 70,
-//       houseCost: 100
-//     }]
-//   }
-// }
-
-//   render() {
-
-//     return (
-//       <div className="container">
-//         <div className="row">
-//           <h3>Monopoly Calculator</h3>
-//           <h6><i>Figure out the net worth of the game winner</i></h6>
-//           <ul className="list-unstyled properties-item">
-//             {this.state.properties.map((property, i) =>
-//               <Property key={i} propertyNode={property} />)}
-//           </ul>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
