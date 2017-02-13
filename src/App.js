0f9b7fffe083d3ab86ae99b6ade3951b9e66040c
Kelly Nelson <nelson20@gmail.com>
2017-02-13 13:43:00 -0800

    style sticky header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import data from './data.json'; // Bring in data from json file
 import PropertyValues from './property-values';
 import PropertyName from './property-name';
 import PropertyValuesTotal from './property-values-total';
-import data from './data.json'; // Bring in data from json file
-import { Header, Navbar, DropdownButton, MenuItem, Form, Grid } from 'react-bootstrap';
+import { PageHeader, Navbar, DropdownButton, MenuItem, Form, Grid } from 'react-bootstrap';
 
 
 class Property extends React.Component {
@@ -36,7 +36,7 @@ class NetWorthTotal extends React.Component {
 
     return (
       <div className="networth-total-box">
-        
+        <h6 className="total-header">TOTAL</h6>
       </div>
     )
   }
@@ -55,17 +55,18 @@ class App extends React.Component {
     });
 
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col-xs-12">
-            <h3 className="kabel-font">MONOPOLY <br/>CALCULATOR</h3>
-            <NetWorthTotal />
-          </div>
+      <div>
+        <div className="app-header">
+          <h3 className="kabel-font-main">MONOPOLY</h3><h4 className="kabel-font-sub">CALCULATOR</h4>
+          <NetWorthTotal />
+          <h6 className="sub-header">Figure out the net worth of the game winner</h6>
         </div>
-        <div className="row">
-          <ul className="list-unstyled properties-item">
-            {propertyNode}
-          </ul>
+        <div className="container">
+          <div className="row property-item-list-container">
+            <ul className="list-unstyled properties-item">
+              {propertyNode}
+            </ul>
+          </div>
         </div>
       </div>
     );
