0bf710e4f78dabb6f79f9db65a0263d44d247fce
Kelly Nelson <nelson20@gmail.com>
2017-02-13 12:37:23 -0800

    add custom font, add final total box

--- src/property-set.js
+++ src/property-set.js
@@ -1,12 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-//import {PropertyValues} from './property-set';
-import data from './data.json'; // Bring in data from json file
 import { Header, Navbar, DropdownButton, MenuItem, Form, Grid } from 'react-bootstrap';
-
-
-// Showing components starting with the bottom in the hierarchy.
 
 class PropertyName extends React.Component { 
   // Child to Property.
@@ -52,6 +45,8 @@ class PropertyValues extends React.Component {
   }
 }
 
+export class PropertyValues {}
+
 class PropertyValuesTotal extends React.Component { 
   // Child to Property.
   // This should show the user the total valuation of the property once they select the drop down menu
@@ -64,69 +59,6 @@ class PropertyValuesTotal extends React.Component {
   }
 }
 
-class Property extends React.Component {
-  // This should be the parent/container for <PropertyName />, <PropertyValues /> and <PropertyValuesTotal />.
-  // This should be a child to <App />
-  render() {
 
-    const { property } = this.props
 
-    return (
-      
-      <div className="container">
-        <div className="row properties-item">
-          <div className="col-xs-12">
-            <PropertyName property={property}/>
-            <PropertyValues property={property}/>
-            <PropertyValuesTotal />
-          </div>
-        </div>
-      </div> 
-    );
-  }
-}
-
-class NetWorthTotal extends React.Component {
-
-  render() {
-
-    return (
-      <div className="networth-total-box">
-        
-      </div>
-    )
-  }
-}
-
-class App extends React.Component {
-  // This is the main container component
-
-  render() {
-
-    const propertyNode = data.properties.map(function(property) {
-      return (
-        <Property key={property.id} property={property} />
-
-      );
-    });
-
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col-xs-12">
-            <h3 className="kabel-font">MONOPOLY <br/>CALCULATOR</h3>
-            <NetWorthTotal />
-          </div>
-        </div>
-        <div className="row">
-          <ul className="list-unstyled properties-item">
-            {propertyNode}
-          </ul>
-        </div>
-      </div>
-    );
-  }
-}
-// <h6><i>Figure out the net worth of the game winner</i></h6>
-export default App;
 
