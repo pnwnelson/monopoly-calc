4be8fca7e0b84ce90f784665f464aaa11934ce18
Kelly Nelson <nelson20@gmail.com>
2017-11-19 21:51:56 -0800

    fix adsense responsive ad

--- src/adsense/ad-sense-responsive.js
+++ src/adsense/ad-sense-responsive.js
@@ -18,18 +18,14 @@ class AdSenseResponsive extends React.Component {
 
 	render () {
 
-		// const style = {
-		// 	display: 'block'
-		// }
-
 		return (
 			<div className="ad-sense">
 				<div className='ad'>
 					<ins className='adsbygoogle ad-sense-ins' 
-						// style={style}
 						data-ad-client={this.props.client}
 						data-ad-slot={this.props.slot}
-						data-ad-format={this.props.format} />
+						// data-ad-format={this.props.format} 
+						/>
 				</div>
 			</div>
 		)
@@ -37,16 +33,3 @@ class AdSenseResponsive extends React.Component {
 }
 
 export default AdSenseResponsive;
-
-
-
-// <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-// <!-- Monopoly Responsive -->
-// <ins class="adsbygoogle"
-//      style="display:block"
-//      data-ad-client="ca-pub-6063578944512286"
-//      data-ad-slot="8034558454"
-//      data-ad-format="auto"></ins>
-// <script>
-// (adsbygoogle = window.adsbygoogle || []).push({});
-// </script>
